872bd63513a8f5ff88a7509c9b8702cd1cb5b491
Tommaso Figlia <tommasofiglia@gmail.com>
2020-11-12 14:50:18 +0100

    Esercizio completato
    Al posto di 19, per le due cifre finali, ho impostato una variabile che attraverso la differenza tra l'anno corrente (Inserito dall'utente)  e la cifra 2000 , restituisse le ultime 2 cifre dell'anno in corso.

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+// Nome cognome colore preferito
+
+// Variabile Nome
+var name = prompt("Qual è il tuo nome?");
+
+// Variabile Cognome
+var surname = prompt("Qual è il tuo cognome?");
+
+// Variabile Colore preferito
+var color = prompt("Qual è il colore che ti piace di più?");
+
+// Variabile Anno corrente (ultime due cifre)
+var currentYear = parseInt(prompt("In che anno siamo?"));
+var lastTwoLetterYear = currentYear - 2000;
+
+
+document.getElementById('password').innerHTML = name + surname + color + lastTwoLetterYear;
